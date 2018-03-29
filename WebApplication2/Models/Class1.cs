d14f9da4996c971fd69a330f90e73f3c410422f9
user <user@MA324-19>
2018-03-29 15:41:45 +0800

    加入專案檔案。

---
+++ WebApplication2/Models/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Class1
+    {
+        public int OrderID{ get; set; }
+
+        public int CustomerID { get; set; }
+
+        public int EmployeeID { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        public DateTime RequireDate { get; set; }
+
+        public DateTime ShippedDate { get; set; }
+
+        public int ShipperID { get; set; }
+
+        public Nullable<Decimal> Freight { get; set; }
+
+        public Char ShipAddress { get; set; }
+
+        public Char ShipCity { get; set; }
+
+        public Char ShipRegion { get; set; }
+
+        public Char ShipPostalCode { get; set; }
+
+        public Char ShipCountry { get; set; }
+        
+    }
+}
